d2ec3c25e2ee26226e802003c1942a32dfcfedbf
samylayang <samylayang@gmail.com>
2026-06-29 11:58:44 +0900

    add some program

--- main.rs
+++ main.rs
@@ -6,4 +6,20 @@ fn main() {
         "goodbye" => println!("Goodbye, world!"),
         _ => println!("Neither hello nor goodbye!"),
     }
+
+    let my_string: String = String::from("hello");
+
+    match my_string.as_str() {
+        "hello" => println!("Hello, world!"),
+        "goodbye" => println!("Goodbye, world!"),
+        _ => println!("Neither hello nor goodbye!"),
+    }
+
+    let my_string: String = String::from("HELLO");
+
+    match my_string.to_lowercase().as_str() {
+        "hello" => println!("Hello, world!"),
+        "goodbye" => println!("Goodbye, world!"),
+        _ => println!("Neither hello nor goodbye!"),
+    }
 }
